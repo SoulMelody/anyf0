a323686800cae3317997b0f07e205b7db2b848f4
SoulMelody <SoulMelody@users.noreply.github.com>
2025-01-04 14:44:41 +0800

    use conda as dependency manager

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-pip install pdm
-pdm config pypi.url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
-pdm sync
-pdm run python -m pip install \
-    --index-url https://pypi.tuna.tsinghua.edu.cn/simple \
-    --find-links https://mirrors.aliyun.com/pytorch-wheels/torch_stable.html \
-    --no-cache-dir \
-    torchaudio===2.1.1+cu121
+mamba env create -n anyf0 --file environment.yml --solver libmamba
+mamba activate anyf0
